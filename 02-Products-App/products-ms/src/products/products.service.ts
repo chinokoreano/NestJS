a5c3cb642b0777c7f9261d8955bfdf03526eac7b
Carlos Romero <67611257+chinokoreano@users.noreply.github.com>
2024-05-05 20:12:04 -0500

    Microservicios

--- 02-Products-App/products-ms/src/products/products.service.ts
+++ 02-Products-App/products-ms/src/products/products.service.ts
@@ -6,46 +6,46 @@ import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
-  private readonly logger = new Logger('ProductsService');
+    private readonly logger = new Logger('ProductsService');
 
-  onModuleInit() {
-    this.$connect();
-    this.logger.log('Database initialized');
-  }
-  create(createProductDto: CreateProductDto) {
-    return this.product.create({
-      data: createProductDto,
-    });
-  }
+    onModuleInit() {
+        this.$connect();
+        this.logger.log('Database initialized');
+    }
+    create(createProductDto: CreateProductDto) {
+        return this.product.create({
+            data: createProductDto,
+        });
+    }
 
-  async findAll(paginationDto: PaginationDto) {
-    const { page, limit } = paginationDto;
-      const totalPage = await this.product.count();
-      const lastPage = Math.ceil(totalPage / limit)
+    async findAll(paginationDto: PaginationDto) {
+        const { page, limit } = paginationDto;
+        const totalPage = await this.product.count();
+        const lastPage = Math.ceil(totalPage / limit)
 
 
-    return {
-      data: await this.product.findMany({
-        skip: (page - 1) * limit,
-        take: limit,
-      }),
-      meta:{
-        page:page,
-        totalPage:page,
-        lastPage:lastPage,        
-      }
-    };
-  }
+        return {
+            data: await this.product.findMany({
+                skip: (page - 1) * limit,
+                take: limit,
+            }),
+            meta: {
+                page: page,
+                totalPage: page,
+                lastPage: lastPage,
+            }
+        };
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} product`;
+    }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
+    update(id: number, updateProductDto: UpdateProductDto) {
+        return `This action updates a #${id} product`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
-  }
+    remove(id: number) {
+        return `This action removes a #${id} product`;
+    }
 }

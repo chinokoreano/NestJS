4b95d3194a8d8bf7ca8d40847cf27aea799c8b86
Carlos Romero <67611257+chinokoreano@users.noreply.github.com>
2024-09-14 17:05:50 -0500

    Microservicios 1era parte

--- 02-Products-App/products-ms/src/main.ts
+++ 02-Products-App/products-ms/src/main.ts
@@ -3,13 +3,23 @@ import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { envs } from './config/env';
 
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 async function bootstrap() {
   const logger = new Logger('Main');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port:envs.port
+      }
+    }
+    
+  );
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(envs.port);
-  logger.log(`Server is running on port ${envs.port}`);
+  await app.listen();
+  logger.log(`Product microservices is running on port ${envs.port}`);
 }
 bootstrap();
--- 02-Products-App/products-ms/src/products/products.service.ts
+++ 02-Products-App/products-ms/src/products/products.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
@@ -20,32 +21,56 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
     async findAll(paginationDto: PaginationDto) {
         const { page, limit } = paginationDto;
-        const totalPage = await this.product.count();
-        const lastPage = Math.ceil(totalPage / limit)
-
+        const totalPages = await this.product.count({ where: {available:true}});
+        const lastPage = Math.ceil(totalPages / limit);
 
         return {
             data: await this.product.findMany({
                 skip: (page - 1) * limit,
                 take: limit,
+                where: {
+                    available:true
+                }
             }),
             meta: {
                 page: page,
-                totalPage: page,
+                total: totalPages,
                 lastPage: lastPage,
-            }
+            },
         };
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} product`;
+    async findOne(id: number) {
+        const product = await this.product.findFirst({
+            where: {
+                id: id,available: true,
+            },
+        });
+        if (!product) {
+            throw new NotFoundException(`Product with id ${id} not found`);
+        }
+        return product;
+    }
+    async update(id: number, updateProductDto: UpdateProductDto) {
+        const{id:__,...data} = updateProductDto;
+        await this.findOne(id);
+        return this.product.update({
+            where: { id },
+            data: data,
+        })
     }
 
-    update(id: number, updateProductDto: UpdateProductDto) {
-        return `This action updates a #${id} product`;
-    }
-
-    remove(id: number) {
-        return `This action removes a #${id} product`;
+    async remove(id: number) {
+        await this.findOne(id);
+        //return this.product.delete({
+        //    where: { id },
+        //})
+        const product = await this.product.update({
+            where: { id },
+            data: { 
+                available: false
+            }
+        })
+        return product;
     }
 }
